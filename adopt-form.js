5f6de1f14ac5f234d012fb5d6be1db49fd4c6fef
Anagha Manoj <anaghasobhamanoj@gmail.com>
2023-09-26 14:46:06 +0530

    updated the js files
     - rehome.js and adopt-form.js for form validation

--- adopt-form.js
+++ adopt-form.js
@@ -2,30 +2,31 @@ document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('myForm');
   
     form.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
-      const fullName = document.getElementById('input-name').value;
-      const email = document.getElementById('input-email').value;
-      const phoneNumber = document.getElementById('input-number').value;
-  
-      const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-  
-      if (fullName === '') {
-        alert('Please enter your full name.');
-        return;
-      }
-  
-      if (!emailRegex.test(email)) {
-        alert('Please enter a valid email address.');
-        return;
-      }
-  
-      if (isNaN(phoneNumber) || phoneNumber.length !== 10) {
-        alert('Please enter a valid 10-digit phone number.');
-        return;
-      }
-  
-      form.submit();
+        event.preventDefault();
+      
+        const fullName = document.getElementById('input-name').value;
+        const email = document.getElementById('input-email').value;
+        const phoneNumber = document.getElementById('input-number').value;
+      
+        const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+      
+        if (fullName.trim() === '') {
+            alert('Please enter your full name.');
+            return;
+        }
+      
+        if (!emailRegex.test(email)) {
+            alert('Please enter a valid email address.');
+            return;
+        }
+      
+        if (isNaN(phoneNumber) || phoneNumber.length !== 10) {
+            alert('Please enter a valid 10-digit phone number.');
+            return;
+        }
+      
+        // If all validations pass, you can submit the form
+        form.submit();
     });
-  });
-  
+});
+
--- rehome.js
+++ rehome.js
@@ -1,17 +1,40 @@
 function validateForm(event) {
     event.preventDefault();
 
+    console.log('Validation started...');
+
     const animalType = document.getElementById('input_select').value;
+    console.log('Animal Type:', animalType);
+
     const breed = document.getElementById('input-breed').value;
+    console.log('Breed:', breed);
+
     const size = document.getElementById('input-size').value;
+    console.log('Size:', size);
+
     const marking = document.getElementById('input-marking').value;
+    console.log('Marking:', marking);
+
     const features = document.getElementById('input-features').value;
+    console.log('Features:', features);
+
     const location = document.getElementById('input-location').value;
+    console.log('Location:', location);
+
     const address = document.getElementById('input-address').value;
+    console.log('Address:', address);
+
     const fullName = document.getElementById('input-name').value;
+    console.log('Full Name:', fullName);
+
     const email = document.getElementById('input-email').value;
+    console.log('Email:', email);
+
     const phoneNumber = document.getElementById('input-number').value;
+    console.log('Phone Number:', phoneNumber);
+
     const image = document.getElementById('customFile').value;
+    console.log('Image:', image);
 
     const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
@@ -73,5 +96,6 @@ function validateForm(event) {
     document.getElementById('myForm').submit();
 }
 
-document.getElementById('myForm').addEventListener('submit', validateForm);
+console.log('Script loaded.');
 
+document.getElementById('myForm').addEventListener('submit', validateForm);
